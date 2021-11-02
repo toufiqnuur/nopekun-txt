24913f8a0dcc309bed238f8117b0e0be467a7b4c
nopekun <51075690+toufiqnuur@users.noreply.github.com>
2021-11-02 18:49:40 +0700

    add accessible names to buttons
    Search & Menu Buttons

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,7 +12,7 @@
 
 <nav class="navbar">
 	<a class="navbar__brand" href="/">Nopekun.txt</a>
-	<button class="navbar__button" type="submit">
+	<button class="navbar__button" type="button" aria-label="search">
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			class="h-6 w-6"
@@ -28,7 +28,7 @@
 			/>
 		</svg>
 	</button>
-	<button class="navbar__button" type="submit">
+	<button class="navbar__button" type="button" aria-label="menu">
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			class="h-6 w-6"
