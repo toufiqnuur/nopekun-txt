0c1c020cd32393a4123478da88f5e276bb867943
toufiqnuur <taufik2017yk@gmail.com>
2021-11-04 17:56:55 +0700

    setup tailwind typography plugin

--- src/layouts/post.svelte
+++ src/layouts/post.svelte
@@ -15,8 +15,10 @@
 
 <div class="flex items-start flex-col md:flex-row md:space-x-3">
 	<div class="mb-6 md:mb-0 md:p-3 md:w-8/12 md:border md:border-black">
-		<h1 class="text-2xl font-semibold">{title}</h1>
-		<slot />
+		<h1 class="text-2xl font-semibold mb-6">{title}</h1>
+		<div class="prose lg:prose-xl">
+			<slot />
+		</div>
 	</div>
 	<div class="md:w-4/12 md:p-3 md:border md:border-black">
 		<p>Ditulis oleh: {author}</p>
